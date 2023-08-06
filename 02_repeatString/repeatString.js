b7c4960856595825045e76d3531d2179155863cb
Ilko <ilkovelija@gmail.com>
2023-08-06 19:37:08 +0200

    Fix code to pass test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word,times) {
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    let text = "";
+    for (let i = 0; i < times; i++) {
+      text = word.repeat(times);
+    }
+    return text;
+  }
 };
 
 // Do not edit below this line
