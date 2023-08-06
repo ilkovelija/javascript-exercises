38c423d7be3996cb730830c618841001aece78b7
Ilko <ilkovelija@gmail.com>
2023-08-06 19:50:32 +0200

    Fix code to pass test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let array = word.split('')
+  let reversed = array.reverse();
+  let joined = reversed.join('');
+  console.log(array, reversed, joined);
+  return joined;
 };
 
 // Do not edit below this line
