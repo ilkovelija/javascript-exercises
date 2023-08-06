0f341571d93861b9cc8669d5441eb292a1023fef
Ilko <ilkovelija@gmail.com>
2023-08-06 20:59:29 +0200

    Fix code to pass until test 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
 const removeFromArray = function() {
+  let argsArray = Array.from(arguments);
 
+  let mainArray = argsArray[0];
+
+  console.log(`Full array is ${argsArray}`);
+  
+  console.log(`Main array is ${mainArray}`);
+
+  for (let i = 1; i < argsArray.length; i++) {
+    
+    let toRemove = argsArray[i];
+
+    console.log(argsArray[i]);
+
+    if (mainArray.includes(argsArray[i]) === true) {
+      
+      let newArrayIndex = mainArray.indexOf(toRemove);
+
+      let newArray = mainArray.splice(newArrayIndex, 1);
+    } else {
+      continue;
+    }
+
+    console.log(mainArray);
+  }
+
+  return mainArray
 };
 
 // Do not edit below this line
