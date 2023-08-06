a45323fdaa9261a5e98f3a560f857d5cba0a1120
Ilko <ilkovelija@gmail.com>
2023-08-06 23:10:49 +0200

    Fix code to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celcius = 5 / 9 * (f - 32);
+  let celciusRounded = Math.round(celcius * 10) / 10;
+  console.log(celciusRounded);
+  return celciusRounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * 9 / 5) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  console.log(fahrenheit);
+  return fahrenheitRounded
 };
 
 // Do not edit below this line
